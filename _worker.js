4289af57d4f145c3ce7209c721e1acd6e51718f0
ColisX <166997734+ColisX@users.noreply.github.com>
2024-10-08 17:02:54 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '8694dbf0-1d10-4927-99a0-bbc0f7f9ad81';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
